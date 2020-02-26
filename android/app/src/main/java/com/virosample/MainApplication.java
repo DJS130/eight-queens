f80e68632fa57998754d39508a26fa6bc471985f
DJS130 <123787412+DJS130@users.noreply.github.com>
2020-02-26 16:57:13 +0000

    rbd: don't leak parent_spec in rbd_dev_probe_parent()

---
+++ android/app/src/main/java/com/virosample/MainApplication.java
@@ -0,0 +1,14 @@
+package com.virosample;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements Rea
